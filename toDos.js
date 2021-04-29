77234720319fed6cfd572730e211d9c8153cf025
sehoon <gsh723@naver.com>
2021-04-29 18:13:59 +0900

    [nomad coder challenge last mission]
    5. HTML, CSS : impending, finished 버튼 생성

--- toDos.js
+++ toDos.js
@@ -3,7 +3,20 @@ const input = form.querySelector('input');
 
 const mission = document.querySelector('.mission');
 
-function getTask(task){ // 위에 추가됨을 띄우고, 2초 후 사라짐
+const tasks = document.querySelector('.tasks');
+const pending = tasks.querySelector('.pending');
+const finished = tasks.querySelector('.finished');
+
+
+function handleClick(){
+  console.log('df');
+}
+
+pending.addEventListener('click', handleClick);
+finished.addEventListener('click', handleClick);
+
+// 위에 추가됨을 띄우고, 2초 후 사라짐
+function getTask(task){ 
   const p = document.createElement('p');
   p.innerHTML = `'${task}'를 추가하셨습니다.`;
   p.classList.add('task');
@@ -13,6 +26,7 @@ function getTask(task){ // 위에 추가됨을 띄우고, 2초 후 사라짐
   },2000);
 }
 
+// input 엔터 누르고 값 받아오기
 function handleSubmit(){
   const task = input.value;  
   getTask(task);
