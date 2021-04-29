ce6e07828f26dec58874b0adb630edcd97209d1e
sehoon <gsh723@naver.com>
2021-04-29 16:37:30 +0900

    [nomad coder challenge last mission]
    3. JavaScript : bg 변경 구현
    : Math.random() 함수를 이용하여, 배경 사진을 랜덤으로 가져오게 했다.

---
+++ bg.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body');
+
+function changeBG(){
+  const bgNum = Math.floor(Math.random()*4)+1;  
+  const image = new Image();
+  image.src = `${bgNum}.jpg`;  
+  image.classList.add('bgImage');
+  body.appendChild(image);
+
+}
+changeBG();
+
--- clock.js
+++ clock.js
@@ -4,7 +4,7 @@ function getTime(){
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
-  clock.innerHTML = `${hours<10?`0${hours}`:hours}:${minutes<10?`${minutes}`:minutes}`;
+  clock.innerHTML = `${hours<10?`0${hours}`:hours}:${minutes<10?`0${minutes}`:minutes}`;
 }
 
 function init(){
