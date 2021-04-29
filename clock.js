773771082fc86e3123ed74d4fd93d53ee4191c20
sehoon <gsh723@naver.com>
2021-04-29 10:53:00 +0900

    [nomad coder challenge last mission]
    2. JavaScript : clock 기능 구현

---
+++ clock.js
@@ -0,0 +1,17 @@
+const clock = document.querySelector('h1');
+
+function getTime(){
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  clock.innerHTML = `${hours<10?`0${hours}`:hours}:${minutes<10?`${minutes}`:minutes}`;
+}
+
+function init(){
+  getTime();
+  setInterval(getTime,60000);
+}
+
+init();
+
+
