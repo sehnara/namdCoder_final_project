d728d54d5e9452bd926f049c28cd6591042bc1f4
sehoon <gsh723@naver.com>
2021-05-03 10:15:27 +0900

    Merge branch 'master' of https://github.com/sehnara/namdCoder_final_project
    # Conflicts:
    #	name.js
    #	weather.js

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 const API_KEY = '6338e402a8624d753acce848f71624c8';
 const divMission = document.querySelector('.mission');
@@ -58,4 +59,66 @@ function loadCoords(){
 function init(){
     loadCoords();
 }
+=======
+
+const API_KEY = '6338e402a8624d753acce848f71624c8';
+const divMission = document.querySelector('.mission');
+const COORDS =  'coords';
+
+function typeWeather(){
+    // const weather = 
+}
+
+function getWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const curTemp = json.main.temp;
+        const feelsLike = json.main.feels_like;
+
+        const para = document.createElement('p');
+        para.classList.add('temp');
+        para.innerHTML = `current temperature is ${curTemp}, feels like ${feelsLike}`;
+        divMission.appendChild(para);
+    })
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);    
+    getWeather(latitude,longitude);
+}
+function handleError(){
+    console.log('Error access GeoLocation')
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords ===null){
+        askForCoords();
+    }
+    else{
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+>>>>>>> de79b9aa56d8b8453508154fa070d6598dec16b1
 init();
